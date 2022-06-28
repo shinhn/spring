8437301d78a7e9e676301ac9786b6b2a00a30c49
peanut <rkdrpgml@naver.com>
2022-06-28 23:13:31 +0900

    도메인 분석 설계

--- inflearn/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ inflearn/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,11 +1,23 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Embeddable;
 
 @Embeddable // JPA 내장 타입 (새로운 값 타입을 직접 정의해서 사용), @Embeddable 와 사용하는 쪽의 @Embedded 둘중 하나만 있어도 되지만 그냥 두개 다 써줌
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
 }
+
+/*
+* 값 타입은 변경 불가능하게 설계해야 한다.
+*
+* @Setter 를 제거하고, 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스를 만들자.
+* JPA 스펙상 엔티티나 임베디드 타입( @Embeddable )은 자바 기본 생성자(default constructor)를 public 또는 protected 로 설정해야 한다.
+* public 으로 두는 것 보다는 protected 로 설정하는 것이 그나마 더 안전하다.
+* JPA가 이런 제약을 두는 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플랙션 같은 기술을 사용할 수 있도록 지원해야 하기 때문이다.
+* */
--- inflearn/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ inflearn/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "order_id")
+@Table(name = "orders")
 @Getter
 @Setter
 public class Order {
@@ -18,14 +18,20 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    /*
+    * 즉시로딩( EAGER )은 예측이 어렵고, 어떤 SQL이 실행될지 추적하기 어렵다. 특히 JPQL을 실행할 때 N+1 문제가 자주 발생한다.
+    * 실무에서 모든 연관관계는 지연로딩( LAZY )으로 설정해야 한다.
+    * 연관된 엔티티를 함께 DB에서 조회해야 하면, fetch join 또는 엔티티 그래프 기능을 사용한다.
+    * @XToOne(OneToOne, ManyToOne) 관계는 기본이 즉시로딩이므로 직접 지연로딩으로 설정해야 한다.
+    * */
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItem = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 상위 엔터티에서 하위 엔터티로 모든 작업을 전파
+    private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // 비즈니스 로직에서 단순히 멤버 로직만 사용하는데 함께 조회하면, 아무리 연관관계가 걸려있다고 해도 손해이다.JPA는 이 문제를 지연로딩 LAZY를 사용해서 프록시로 조회하는 방법으로 해결 한다.
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +39,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // [ORDER, CANCEL]
+
+    // 연관관계 메서드 (양쪽을 한코드로 세팅하기 좋음)
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
