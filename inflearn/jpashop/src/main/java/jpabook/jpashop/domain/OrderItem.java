abe304ac91f3ede732ef7905a775bfb6f8857aa6
peanut <rkdrpgml@naver.com>
2022-06-30 20:30:40 +0900

    주문 도메인 개발

--- inflearn/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ inflearn/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,14 +1,18 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 일반적인 생성 new를 못쓰게 함 -> 아래에서 만든 생성 메서드를 통해서만 생성할 수 있게 제한함
 public class OrderItem {
     @Id
     @GeneratedValue
@@ -25,4 +29,27 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    // 생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // 비즈니스 로직
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    // 조회 로직
+    // 주문상품 전체 가격 조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
