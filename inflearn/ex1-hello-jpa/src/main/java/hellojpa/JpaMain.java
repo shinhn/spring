459048090b7d284152de86e16e22504a750d1a4a
peanut <rkdrpgml@naver.com>
2022-07-18 15:56:05 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 프록시와 연관관계 관리

--- inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,21 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedTime(LocalDateTime.now());
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            em.persist(member);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
 
             em.flush();
             em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
 
             tx.commit();
         } catch (Exception e){
