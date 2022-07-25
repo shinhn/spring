03bf11f9dfd5e25fe8797fd5f664c70a7f8ac17e
peanut <rkdrpgml@naver.com>
2022-07-25 19:19:53 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 값 타입

--- inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ inflearn/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,11 +17,37 @@ public class JpaMain {
 
         try {
 
+            // <저장>
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
 
+            member.getFaveriteFoods().add("치킨");
+            member.getFaveriteFoods().add("족발");
+            member.getFaveriteFoods().add("피자");
 
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            // <조회>
+            Member findMember = em.find(Member.class, member.getId());
+
+            // <수정>
+            // findMember.getHomeAddress().setCity("newCity); -> 불변 객체이므로 불가능
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            findMember.getFaveriteFoods().remove("치킨");
+            findMember.getFaveriteFoods().add("한식");
 
             tx.commit();
         } catch (Exception e){
+            e.printStackTrace();
             tx.rollback();
         } finally {
             em.close();
--- inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ inflearn/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,10 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -21,11 +25,60 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
-            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
-            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
-    })
-    private Address workAddress;
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> faveriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public Set<String> getFaveriteFoods() {
+        return faveriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public void setFaveriteFoods(Set<String> faveriteFoods) {
+        this.faveriteFoods = faveriteFoods;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
 }
