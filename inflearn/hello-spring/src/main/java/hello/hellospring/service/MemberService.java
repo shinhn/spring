2cd152e2b4f78f191a3ff8d54d8fa5e2a846a8e6
shinhn <rkdrpgml@naver.com>
2022-08-03 14:50:19 +0900

    [스프링 입문] 스프링 빈과 의존관계

--- inflearn/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ inflearn/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.Repository.MemberRepository;
 import hello.hellospring.Repository.MemoryMemberRepository;
 import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
