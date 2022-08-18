d67f18d2aff9a06537f731c340fc27ed842e3c40
shinhn <rkdrpgml@naver.com>
2022-08-18 18:28:25 +0900

    [스프링 핵심 원리 - 기본편] 스프링 컨테이너와 스프링 빈

--- inflearn/1-2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderApp.java
+++ inflearn/1-2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderApp.java
@@ -4,7 +4,6 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
