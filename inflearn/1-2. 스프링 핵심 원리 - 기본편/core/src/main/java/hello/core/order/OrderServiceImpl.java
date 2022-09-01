243f18be4b8c3256961be054996ffafc139cdc3f
shinhn <rkdrpgml@naver.com>
2022-09-01 16:29:50 +0900

    [스프링 핵심 원리 - 기본편] 의존관계 자동 주입

--- inflearn/1-2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ inflearn/1-2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,14 +9,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
+    // 의존관계 주입 - 생성자 주입
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    // 의존관계 주입 - 수정자 주입
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+    // 의존관계 주입 - 필드 주입
+    /*
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private DiscountPolicy discountPolicy;
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
